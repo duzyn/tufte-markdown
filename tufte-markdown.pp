16a5d0da85bb76d97c1501b0537da1767672e2de
David Peng <davidduzyn@outlook.com>
2020-04-18 14:52:19 +0800

    add video and audio

--- tufte-markdown.pp
+++ tufte-markdown.pp
@@ -115,8 +115,9 @@
 !define(fullwidthfigure)(
 !ifeq(!format)(html)(
 <figure class="fullwidth">
-<img src="!2" alt="!1">
+<img src="!2" alt="!1"/>
 </figure>
+<span class="marginnote">!1</span>
 )
 
 !ifeq(!format)(pdf)(
@@ -147,17 +148,88 @@
 
 
 !comment(
+// Video (Only for HTML)
+// Usage: !video(CAPTION)(FILENAME1)(FILENAME2)(FILENAME3)(FILENAME4)
+// Use html5media: https://github.com/etianen/html5media
+// <script src="https://cdn.jsdelivr.net/npm/html5media@1.2.1/dist/api/1.2.1/html5media.min.js"></script>
+)
+
+!define(VIDEO)(1)
+!define(video)(
+!ifeq(!format)(html)(
+<div>
+    <label for="video-!VIDEO" class="margin-toggle">&#8853;</label>
+    <input type="checkbox" id="video-!VIDEO" class="margin-toggle"/>
+    <span class="marginnote">!1</span>
+    <video controls>
+        <source src="!2">
+        !ifdef(3)(<source src="!3">)
+        !ifdef(4)(<source src="!4">)
+        !ifdef(5)(<source src="!5">)
+    </video>
+</div>
+!add(VIDEO)
+)
+)
+
+
+!comment(
+// Fullwidth Video (Only for HTML)
+// Usage: !fullwidthvideo(CAPTION)(FILENAME1)(FILENAME2)(FILENAME3)(FILENAME4)
+)
+
+!define(fullwidthvideo)(
+!ifeq(!format)(html)(
+<div>
+    <video controls class="fullwidth">
+        <source src="!2">
+        !ifdef(3)(<source src="!3">)
+        !ifdef(4)(<source src="!4">)
+        !ifdef(5)(<source src="!5">)
+    </video>
+</div>
+<span class="marginnote">!1</span>
+)
+)
+
+
+!comment(
+// Audio (Only for HTML)
+// Usage: !audio(CAPTION)(FILENAME1)(FILENAME2)(FILENAME3)(FILENAME4)
+// Use html5media: https://github.com/etianen/html5media
+// <script src="https://cdn.jsdelivr.net/npm/html5media@1.2.1/dist/api/1.2.1/html5media.min.js"></script>
+)
+
+!define(AUDIO)(1)
+!define(audio)(
+!ifeq(!format)(html)(
+<div>
+    <label for="audio-!AUDIO" class="margin-toggle">&#8853;</label>
+    <input type="checkbox" id="audio-!AUDIO" class="margin-toggle"/>
+    <span class="marginnote">!1</span>
+    <audio controls>
+        <source src="!2">
+        !ifdef(3)(<source src="!3">)
+        !ifdef(4)(<source src="!4">)
+        !ifdef(5)(<source src="!5">)
+    </audio>
+</div>
+!add(AUDIO)
+)
+)
+
+!comment(
 // Table
 // Usage: !table(CSV_FILENAME)(CAPTION)
 )
 
 !define(table)(
 !ifeq(!format)(html)(
-<section>
+<div>
 <span class="marginnote">!2</span>
 
 !csv(!1)
-</section>
+</div>
 )
 
 !ifeq(!format)(pdf)(
